e6b3e5b043bc0c23bbbc01947c2666634342c561
andookie <156530133+andydeng428@users.noreply.github.com>
2024-07-18 14:57:25 -0400

    Update ChessGame.h

--- include/ChessGame.h
+++ include/ChessGame.h
@@ -1,25 +1,30 @@
-#ifndef CHESS_ANDY_CHESSGAME_H
-#define CHESS_ANDY_CHESSGAME_H
+#ifndef CHESSGAME_H
+#define CHESSGAME_H
 
+#include <SFML/Graphics.hpp>
 #include "Board.h"
-#include "ChessRuleBook.h"
+#include "Piece.h"
+#include "Color.h"
 
 class ChessGame {
 public:
     ChessGame();
     void startGame();
+    void drawBoard(sf::RenderWindow &window);
+    void handleClick(int x, int y);
 
 private:
-    Color currentPlayer;
-    Board board;
-    ChessRuleBook ruleBook;
-
     void initializeBoard();
     bool performMove(int startX, int startY, int endX, int endY);
     bool isCheckmate(Color playerColor);
     bool isStalemate(Color currentPlayer, const Board& board);
-    bool isGameEnded();
-    void displayWinner();
+    bool isGameEnded(Color currentPlayer, const Board& board);
+    bool isKingInCheck(Color playerColor, int kingX, int kingY);
+
+    Board board;
+    Color currentPlayer;
+    bool selected;
+    int selectedX, selectedY;
 };
 
-#endif //CHESS_ANDY_CHESSGAME_H
+#endif // CHESSGAME_H
